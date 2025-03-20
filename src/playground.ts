5b964b2819b53aaeeb3ca17446d01e50aa620d04
Axel Rauschmayer <axel@rauschma.de>
2025-03-20 15:32:59 +0100

    Switch to pkg.license (pkg.version doesn’t exist)

--- src/playground.ts
+++ src/playground.ts
@@ -1,7 +1,5 @@
 // Use this file to interactively try out TypeScript
-// Details: https://github.com/rauschma/nodejs-type-stripping#using-srcplaygroundts-as-a-simple-typescript-playground
 
-import pkg from '../package.json' with {type: 'json'};
+import pkg from '../package.json' with { type: 'json' };
 
-console.log('Version of this package: ' + pkg.version);
-console.log('YES');
+console.log('License of this package: ' + pkg.license);
